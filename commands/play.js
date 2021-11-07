096359525c6746af7e87c6ebff2b52d5b026662a
Ricardo271 <ric.zanluchi@gmail.com>
2021-11-07 13:08:53 -0300

    Testing

--- commands/play.js
+++ commands/play.js
@@ -10,7 +10,7 @@ module.exports = {
         .setName('play')
         .setDescription('Plays a youtube video')
         .addStringOption(option =>
-            option.setName('link')
+            option.setName('request')
                 .setDescription('Link or search term')
                 .setRequired(true)),
     async execute(interaction) {
@@ -20,5 +20,61 @@ module.exports = {
 
         const server_queue = queue.get(interaction.guildId);
 
+        let song = {};
+
+        const request = interaction.options.getString('request');
+        if (ytdl.validateURL(request)) {
+            const song_info = await ytdl.getInfo(request);
+            song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url };
+        } else {
+            const video_finder = async (query) => {
+                const video_result = await ytSearch(query);
+                return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+            }
+
+            const video = await video_finder(request);
+            if (video) {
+                song = { title: video.title, url: video.url };
+            } else {
+                interaction.reply('Error finding video.');
+            }
+        }
+
+        if (!server_queue) {
+
+            const queue_constructor = {
+                voice_channel: voice_channel,
+                text_channel: interaction.channel,
+                connection: null,
+                songs: []
+            }
+
+            queue.set(interaction.guildId, queue_constructor);
+            queue_constructor.songs.push(song);
+
+            try {
+                const connection = await voice_channel.join();
+                queue_constructor.connection = connection;
+                video_player(interaction.guild, queue_constructor.songs[0]);
+            } catch (error) {
+                queue.delete(interaction.guildId);
+                interaction.reply('There was an error connecting.');
+                throw error;
+            }
+        } else {
+            server_queue.songs.push(song);
+            return interaction.reply(`**${song.title}** added to queue!`);
+        }
+
     },
 };
+
+const video_player = async (guild, song) => {
+    const song_queue = queue.get(guild.id);
+
+    if (!song) {
+        queue.delete(quild.id);
+        return;
+    }
+    const stream = ytdl(song.url, { filter: 'audioonly' });
+}
